0e8c69fd0d73fd21ca4551efbd5d47d158cf0d7a
olegkleiman <oleg_kleyman@hotmail.com>
2023-12-16 03:46:57 +0200

    .

--- Odyssey/Program.cs
+++ Odyssey/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Ardalis.GuardClauses;
 using Azure;
-using Azure.AI.OpenAI;
+//using Azure.AI.OpenAI;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Odyssey.Models;
@@ -11,95 +11,6 @@ namespace Odyssey
 {
     internal class Program
     {
-        private static void SaveAndEmbed(Doc doc)
-        {
-            if (doc == null)
-                return;
-
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false);
-            IConfiguration config = builder.Build();
-
-            var connectionString = config.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
-            if (string.IsNullOrEmpty(connectionString))
-                throw new ApplicationException("No connection string when saving scrapped document");
-
-            var openaiKey = config["OPENAI_KEY"];
-            if( string.IsNullOrEmpty(openaiKey) )
-                throw new ApplicationException("No OPENAI key for embedding scrapped document");
-
-            int rowId = saveDoc(doc, connectionString);
-            if (rowId > 0)
-            {
-                doc.Id = rowId;
-                Embed(doc, connectionString, openaiKey);
-            }
-        }
-
-        private static void Embed(Doc doc, 
-                                  string connectionString,
-                                  string providerKey)
-        {
-            try
-            {
-                // Azure OpenAI package
-                var client = new OpenAIClient(providerKey);
-                string? _content = doc.Content;
-                if (string.IsNullOrEmpty(_content))
-                    return;
-
-                using var conn = new SqlConnection(connectionString);
-                conn.Open();
-
-                SqlBulkCopy objbulk = new SqlBulkCopy(conn);
-                objbulk.DestinationTableName = "site_docs_vector";
-                objbulk.ColumnMappings.Add("doc_id", "doc_id");
-                objbulk.ColumnMappings.Add("vector_value_id", "vector_value_id");
-                objbulk.ColumnMappings.Add("vector_value", "vector_value");
-                objbulk.ColumnMappings.Add("model_id", "model_id");
-
-                //List<string> inputs = new List<string>();
-                //inputs.Add(_content);
-
-                EmbeddingsOptions eo = new EmbeddingsOptions()
-                {
-                    DeploymentName = "text-embedding-ada-002",
-                    Input = [_content]
-                };
-
-                Response<Embeddings> response = client.GetEmbeddings(eo);
-
-                DataTable tbl = new();
-                tbl.Columns.Add(new DataColumn("doc_id", typeof(int)));
-                tbl.Columns.Add(new DataColumn("vector_value_id", typeof(int)));
-                tbl.Columns.Add(new DataColumn("vector_value", typeof(float)));
-                tbl.Columns.Add(new DataColumn("model_id", typeof(int)));
-
-                foreach (var item in response.Value.Data)
-                {
-                    var embedding = item.Embedding;
-                    for (int i = 0; i < embedding.Length; i++)
-                    {
-                        float value = embedding.Span[i];
-
-                        DataRow dr = tbl.NewRow();
-                        dr["doc_id"] = doc.Id;
-                        dr["vector_value_id"] = i;
-                        dr["vector_value"] = value;
-                        dr["model_id"] = 1;
-
-                        tbl.Rows.Add(dr);
-                    }
-                }
-
-                objbulk.WriteToServer(tbl);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
 
         private static int saveDoc(Doc doc, string connectionString)
         {
